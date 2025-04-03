7d8da4315f19bdd265c104d66ab7fce8700cda51
yeferHome <yefersonrojas16yi@gmai.com>
2025-04-03 10:13:42 -0500

    Micro Servicio de Trazabilidad con dependencias

---
+++ MicroTrazabilidad/src/main/java/retoPragma/MicroTrazabilidad/MicroTrazabilidadApplication.java
@@ -0,0 +1,13 @@
+package retoPragma.MicroTrazabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTrazabilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTrazabilidadApplication.class, args);
+	}
+
+}
---
+++ MicroTrazabilidad/src/test/java/retoPragma/MicroTrazabilidad/MicroTrazabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package retoPragma.MicroTrazabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTrazabilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
